6d4d00df875abcc1dd34cc36b2e7386a5fa8cb18
jefferds <jefferds@gmail.com>
2022-04-08 08:52:33 -0300

    first commit

---
+++ Plano da Disciplina.jl
@@ -0,0 +1,117 @@
+### A Pluto.jl notebook ###
+# v0.19.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e6e24892-e2b7-472f-9a5c-7d1cbe34eeb6
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ f92f9d30-b368-11eb-2e66-a7bbf5a4a943
+md"""
+# Plano da disciplina de Linguagens de Programação
+
+## Ementa da Disciplina:
+
+1. **Noções Fundamentais:** computador, comandos básicos, estruturas de controle, estruturas de dados, sistemas operacionais, linguagem de programação.
+
+2. **Algoritmo e fluxograma:** conceito, representação formal e desenvolvimento estruturado.
+
+3. **Programas:** conceito, desenvolvimento sistemático. Metodologia de desenvolvimento de programas, programação em linguagem de alto nível. Prática de desenvolvimento de programas.
+
+## Resultados de aprendizagem esperados
+
+*🥇 _**Identificar e utilizar computadores no desenvolvimento de atividades de Engenharia de Materiais**_.
+
+*🥈 _**Elaborar programas simples de computador**_.
+
+## Metodologia
+
+### Estratégia
+
+👨‍🔬 Apresentação e orientação no desenvolvimento de projeto por meio de unidade de aprendizagem.
+
+🧑‍🎓 Desenvolvimento das atividades propostas e entrega de relatórios.
+
+## Roteiro
+
+🕐 1. O professor anuncia por meio de uma orientação por escrito:
+
+🕑 2. Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de uma página do que estudou.
+
+🕓 3. Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade?.
+
+🕕 4. Desenvolvimento da atividade proposta.
+
+🕛 5. Envio via Classroom do relatório da atividade para postagem na internet.
+
+🕑 6. Avaliações.
+
+🕒 7. Feedback.
+
+## Recursos e Materiais Didáticos
+
+1. Google Classroom;
+3. Os recurso $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>").
+
+4. Página do professor para registro dos casos.
+
+## Indicações de Referências
+
+### Bibliografia
+
+1. ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. _**Fundamentos da programação de computadores**_. Pearson Educación, 2008.
+
+2. Documentação do [Julia](https://docs.julialang.org/en/v1/)
+
+3. Artigos e Blogs na Internet.
+
+### Bibliografia (PPC):
+
+4. MEDINA, M.; FERTIG, C. _**Algoritmos e Programação: Teoria e Prática**_. 2. ed. São Paulo: Novatec Editora, 2005.
+
+5. FARRER, H. et al.: _**Algoritmos Estruturados**_. São Paulo: LTC, 1999.
+
+6. NORTON, P. _**Introdução a Informática**_. Pearson Education do Brasil, 2010.
+
+### Bibliografia Complementar  (PPC):
+
+1. MECLER, I., MAIA, L.P.: _**Programação e Lógica com Turbo Pascal**_. Rio de Janeiro: Campus, 1989.
+
+2. GOTTFRIED, B.S.: _**Programação em Pascal**_. São Paulo: McGraw-Hill (Coleção Schaum), 1988.
+
+3. MIZRAHI, V.V. _**Treinamento em Linguagem C: Curso Completo em Um Volume**_. 2. ed. Pearson Education, 2008. 432 p. ISBN: 9788576051916
+
+4. FORBELLONE, A. et al. _**Lógica de programação: a construção de algoritmos e estruturas de dados**_. Rio de Janeiro: Makron Books, 1993.
+
+5. GUIMARÃES, A.M., LAGES, N.A.C. _**Algoritmos e estrutura de dados**_. Rio de Janeiro: LTC, 1994.
+
+## Procedimento de Avaliação da Apredizagem
+
+Avaliação será:
+
+- Contínua - Individual - Por atividade a ser desenvolvida (60% da nota)
+- Projeto Final (40% da nota)
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─e6e24892-e2b7-472f-9a5c-7d1cbe34eeb6
+# ╠═f92f9d30-b368-11eb-2e66-a7bbf5a4a943
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
