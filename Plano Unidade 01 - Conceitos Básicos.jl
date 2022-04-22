7e1e186c6df6d4b6804dc2acff5cc45e467647b4
jefferds <jefferds@gmail.com>
2022-04-22 08:20:54 -0300

    Unidade 01

---
+++ Plano Unidade 01 - Conceitos Básicos.jl
@@ -0,0 +1,98 @@
+### A Pluto.jl notebook ###
+# v0.19.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 5ccbcf08-3fde-492a-9951-907168ac4300
+md"""
+## Plano da Unidade 01 - Conceitos Bá
+"""
+
+# ╔═╡ 660d3c19-f9ee-4edc-b167-ba60fd3e554d
+md"""
+### Tema relacionado:
+
+1. Conceitos primários: Algoritmo, programa, linguagem de programação, compilador.
+
+- 1.1 Conceito de algoritmo
+- 1.2 Método para a construção de algoritmos
+- 1.3 Tipos de algoritmos
+
+_Fonte:_
+
+- ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. **Fundamentos da programação de computadores.** Pearson Educación, 2008.
+"""
+
+# ╔═╡ 432b2889-ed9b-48af-b56e-9e6d0e0a7b92
+md"""
+### Resultados de aprendizagem esperados
+
+- Compreendeer e aplicar os conceitos de algoritmo, programa e linguagem de programação. 
+- Utilizar as ferramentas $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>") e $(html"<a href='https://github.com/fonsp/Pluto.jl' target='_blank'>Pluto.jl</a>") para simulações e apresentação de relatórios.
+- Discutir duas tarefas entre as principais dos engenheiros, a saber, simulações e cálculos.
+- Desenvolver estratégias para solução de problemas.
+- Estabelecer relações interpessoais - Trabalhar em equipe - Comunicar-se oralmente -Ter responsabilidade coletiva.
+
+"""
+
+# ╔═╡ a13ac61a-6a82-4ac1-b529-84f5f610b354
+md"""
+### Roteiro
+
+### 1. A atividade a ser desenvolvida:
+
+- Identificar um problema e implementar um modelo computacional que permita o cálculo em linguagem Julia, utilizando vscode e Pluto.jl.
+
+- Na produção e apresentação do relatório discutir, os conceitos de Algoritmo, programa, linguagem de programação e compilador.
+
+### 2. Postar no Classroom resumo de uma página do tema
+
+- Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de até uma página do que estudou;
+
+### 3. Relatório e apresentação
+
+- Envio via Classroom do relatório da atividade.
+- Apresentações de cada um dos grupos.
+- Avaliações.
+- Feedback.
+
+"""
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+
+### Bibliografia.
+
+* ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. Fundamentos da programação de computadores. Pearson Educación, 2008.
+
+* Pesquisa a rede computadores. [As fontes devem ser formalmente citadas na norma da ABNT](http://www.biblioteca.fsp.usp.br/~biblioteca/guia/a_modelos.htm).
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─5ccbcf08-3fde-492a-9951-907168ac4300
+# ╟─660d3c19-f9ee-4edc-b167-ba60fd3e554d
+# ╟─432b2889-ed9b-48af-b56e-9e6d0e0a7b92
+# ╟─a13ac61a-6a82-4ac1-b529-84f5f610b354
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
